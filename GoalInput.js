5b8a906d23b6598e3126f7a60c714e6a837e3ae4
Azrahely C. Cordero <166565767+zrhlycc@users.noreply.github.com>
2024-05-02 16:34:05 -0700

    Create GoalInput.js

---
+++ GoalInput.js
@@ -0,0 +1,54 @@
+import { useState } from "react"
+import { View, TextInput, Button, StyleSheet } from "react-native"
+
+function GoalInput({onAddGoal}) {
+    const [enteredGoalText, setEnteredGoalText] = useState('')
+
+    function handleInputGoal(enteredText) {
+        console.log(enteredText)
+        setEnteredGoalText(enteredText)
+    }
+
+    function addGoalHandler() {
+        onAddGoal(enteredGoalText)
+        setEnteredGoalText('')
+    }
+
+    return(
+        <View style={styles.inputContainer}>
+            <TextInput
+                style={styles.textInput}
+                placeholder='Your Goal!'
+                onChangeText={handleInputGoal}
+                value={enteredGoalText}
+            />
+            <Button
+                title='Add Goal'
+                color={'#A3FFD5'}
+                onPress={addGoalHandler}
+            />
+        </View>
+    )
+}
+
+export default GoalInput
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 20,
+        borderBottomwidth: 1,
+        borderBottomColor: '#7BC9FF'
+    },
+    textInput: {
+        borderwidth: 1,
+        borderColor: '#cccccc',
+        width: '80%',
+        marginRight: 3,
+        padding: 8,
+        borderRadius: 5
+    }
+})
