f8515e3c6dac3b5c179d73b67d148fb903ffc60b
Azrahely C. Cordero <166565767+zrhlycc@users.noreply.github.com>
2024-05-07 16:05:50 -0700

    Update GoalItem.js

--- GoalItem.js
+++ GoalItem.js
@@ -1,26 +1,27 @@
-import { StyleSheet, View, Text, Pressable } from "react-native"
+import { StyleSheet, View, Text, Pressable} from"react-native"
 
 function GoalItem({itemData, OnDeleteItem}){
-    return(
-        <Pressable>
-            <View style={StyleSheet.goalsItem}>
-                <Text style={styles.goalsItem}></Text>
-            </View>
-        </Pressable>
-    )
+return(
+    <Pressable onPress={OnDeleteItem}>
+        <View style={styles.goalsItem}>
+            <Text style={styles.goalText}>{itemData.item}</Text>
+
+        </View>
+    </Pressable>
+)
 }
 
 export default GoalItem
 
-const styles= StyleSheet.create({
-    goalsItem: {
-        margin: 8,
-        padding: 8,
-        borderRadius: 6,
-        backgroundColor: '#8576FF',
-        color: 'white'
-    },
-    goalText: {
-        color: 'white'
-    }
+const styles = StyleSheet.create({ 
+goalsItem: {
+    margin:8,
+    padding:8,
+    border:6,
+    backgroundColor: "#8576FF",
+    color: "white"
+},
+goalText:{
+    color:"white"
+}
 })
