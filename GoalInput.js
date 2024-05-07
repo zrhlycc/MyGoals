f53c188a4ef40085a64d68b36994ea8ad72f3da2
Azrahely C. Cordero <166565767+zrhlycc@users.noreply.github.com>
2024-05-07 16:05:27 -0700

    Update GoalInput.js

--- GoalInput.js
+++ GoalInput.js
@@ -1,54 +1,53 @@
 import { useState } from "react"
-import { View, TextInput, Button, StyleSheet } from "react-native"
+import { View, TextInput, Button, StyleSheet} from "react-native"
 
-function GoalInput({onAddGoal}) {
-    const [enteredGoalText, setEnteredGoalText] = useState('')
+function GoalInput({onAddGoal}){
+    const [enteredGoalText, setEnteredGoalText] = useState("")
 
-    function handleInputGoal(enteredText) {
+    function handleInputGoal(enteredText){
         console.log(enteredText)
         setEnteredGoalText(enteredText)
     }
-
-    function addGoalHandler() {
+    function addGoalHandler(){
         onAddGoal(enteredGoalText)
-        setEnteredGoalText('')
+        setEnteredGoalText("")
+        console.log("addGoalHandler")
     }
-
     return(
-        <View style={styles.inputContainer}>
-            <TextInput
-                style={styles.textInput}
-                placeholder='Your Goal!'
-                onChangeText={handleInputGoal}
-                value={enteredGoalText}
-            />
-            <Button
-                title='Add Goal'
-                color={'#A3FFD5'}
-                onPress={addGoalHandler}
-            />
-        </View>
-    )
-}
+        <View style={StyleSheet.inputContainer}>
+           <TextInput
+           style={StyleSheet.textInput}
+           placeholder="Your Goal!"
+           onChangeText={handleInputGoal} 
+           value={enteredGoalText}
+           />
+           <button
+           title="Add Goal"
+           color={"#A3FFD6"}
+           onPress={addGoalHandler}
+           />
+           </View>
 
+    )
+    }
 export default GoalInput
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
+    inputContainer:{
+        flex:1,
+        flexDirection:"row",
+        justifyContent: "center",
+        alignItems: "center",
         marginBottom: 20,
-        borderBottomwidth: 1,
-        borderBottomColor: '#7BC9FF'
+        borderBottomWidth:1,
+        borderBottomColor: "#7BC9FF"
     },
-    textInput: {
-        borderwidth: 1,
-        borderColor: '#cccccc',
-        width: '80%',
-        marginRight: 3,
-        padding: 8,
+    textInput:{
+        borderWidth:1,
+        borderColor: "#cccccc",
+        width:"80%",
+        marginRight:3,
+        padding:8,
         borderRadius: 5
-    }
+    },
 })
